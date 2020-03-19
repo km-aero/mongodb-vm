c8f81fef307063783a6973b7f044baa50cb2a331
Kevin Monteiro <kevin.monteiro96@gmail.com>
2020-03-19 14:24:58 +0000

    initial commit

---
+++ mongoprovisions.sh
@@ -0,0 +1,6 @@
+#/!bin/#!/usr/bin/env bash
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 68818C72E52529D4
+sudo echo "deb http://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
